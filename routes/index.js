4f17ccf32722ff864b9694de8ea3bebb86264747
Camille Guillard <camille-guillard@outlook.fr>
2021-06-08 03:47:01 +0200

    Models + Refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 const userRoutes = require('./user.routes');
 const authRoutes = require('./auth.routes');
 const router = require('express').Router();
-const { ensureAuthenticated } = require('../config/security.config');
 
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
 
-router.get('/protected', ensureAuthenticated, (req, res) => {
-  res.render('protected');
-})
-
 router.get('/',  (req, res) => {
-  res.render('index', { user: req.user });
+  req.isAuthenticated()
+    ? res.render('index', { user: req.user })
+    : res.redirect('auth/signin/form')
 });
 
 module.exports = router;
